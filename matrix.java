f166a9874e2a030bafa3959ca38387c56b30917a
prabhav1998 <64685442+prabhav1998@users.noreply.github.com>
2022-03-20 15:26:11 +0530

    Add files via upload

---
+++ matrix.java
@@ -0,0 +1,74 @@
+// Java program for addition of two matrices
+
+import java.io.*;
+
+class matrix {
+    int a[][] = new int[3][3];
+    int b[][] = new int[3][3];
+    // int c[][]=new int [3][3];
+    int rows, colm;
+
+    matrix() {
+    }// default constractor
+
+    matrix(int x[][],int y[][]) {
+        this.a = x;
+        this.b = y;
+    }// parameterized constractor
+
+    // Function to print Matrix
+    static int[][] printMatrix(int M[][],
+            int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++)
+                System.out.print(M[i][j] + " ");
+
+            System.out.println();
+        }
+        return M;
+    }
+
+    // Function to add the two matrices
+    // and store in matrix C
+    static int[][] add(int A[][], int B[][],
+            int size) {
+        int i, j;
+        int C[][] = new int[size][size];
+
+        for (i = 0; i < size; i++)
+            for (j = 0; j < size; j++)
+                C[i][j] = A[i][j] + B[i][j];
+
+        return printMatrix(C,size);
+    }
+
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int A[][] = new int[3][3];
+        int B[][] = new int[3][3];
+        int size = 3;
+        matrix x ;
+        System.out.println("enter Matrix A:");
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.println("Enter Matrix element"+"["+i+"]"+"["+j+"]:");
+                A[i][j] = Integer.parseInt(reader.readLine());
+            }
+        }
+        System.out.println("Enterd Matrix A is:");
+        printMatrix(A, size);
+
+        System.out.println("enter Matrix B:");
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.println("Enter Matrix element"+"["+i+"]"+"["+j+"]:");
+                B[i][j] = Integer.parseInt(reader.readLine());
+            }
+        }
+        System.out.println("Enterd Matrix B is:");
+        printMatrix(B, size);
+        System.out.println("________________");
+        add(A,B, size);
+    }
+}
